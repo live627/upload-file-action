bc63cc0bb89f15e40fa7c62a00c9a300e1264cb5
John Rayes <live627@gmail.com>
2021-09-22 04:56:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,61 +1,71 @@
-const core = require('@actions/core');
-const uploadFile = require('./uploadFile');
+import { request } from 'http';
+import { readFile } from 'fs';
 
-async function main() {
-  try {
-    // inputs from action
-    const url = core.getInput('url');
-    const methodInput = core.getInput('method');
-    const method = methodInput.toLowerCase();
-    const forms = core.getInput('forms');
-    const formsMap = jsonToMap(forms);
-    const fileForms = core.getInput('fileForms');
-    const fileFormsMap = jsonToMap(fileForms);
+const httpsPost = (options, body) => new Promise((resolve,reject) =>
+{
+	const req = request(options, res =>
+	{
+		let data = '';
+		res.on('data', chunk => data += chunk);
+		res.on('end', () =>
+		{
+			res.data = data;
+			resolve(res);
+		});
+	});
+	req.on('error',reject);
+	req.write(body);
+	req.end();
+});
 
-    console.log(forms);
-    console.log(fileForms);
+const getInput = x => (process.env['INPUT_' + x] || '').trim();
 
-    // http request to external API
-    const response = await uploadFile(url, formsMap, fileFormsMap);
+readFile(getInput('upfile'), (err, content) =>
+{
+	if (err)
+		console.error(err);
 
-    const statusCode = response.status;
-    const data = response.data;
-    const outputObject = {
-      url,
-      method,
-      statusCode,
-      data
-    };
-
-    const consoleOutputJSON = JSON.stringify(outputObject, undefined, 2);
-    console.log(consoleOutputJSON);
-
-    if (statusCode >= 400) {
-      core.setFailed(`HTTP request failed with status code: ${statusCode}`);
-    } else {
-      const outputJSON = JSON.stringify(outputObject);
-      core.setOutput('output', outputJSON);
-    }
-  } catch (error) {
-    console.log(error);
-    core.setFailed(error.message);
-  }
-}
-
-
-function objToStrMap(obj){
-  let strMap = new Map();
-  for (let k of Object.keys(obj)) {
-    strMap.set(k,obj[k]);
-  }
-  return strMap;
-}
-/**
- *json转换为map
- */
-function jsonToMap(jsonStr){
-  return objToStrMap(JSON.parse(jsonStr));
-}
-
-
-main();
+	const boundary = 'xxxxxxxxxx';
+	const boundaryString =  `--${boundary}\r\n`;
+	const data = [
+		boundaryString,
+		Object.entries(getInput('metadata').split("\n").filter(x => x !== ""))
+			.map(([key, val]) => `Content-Disposition: form-data; name="${key}"; \r\n\r\n${val}\r\n`)
+			.join(boundaryString),
+		boundaryString,
+		`Content-Disposition: form-data; name="upfile"; filename="${upfile}"\r\n`,
+		'Content-Type: application/octet-stream\r\n\r\n'
+	];
+	httpsPost(
+		{
+			hostname: getInput('hostname');
+			path: getInput('path');
+			method: getInput('method');
+			headers: {
+				'Content-Type': `multipart/form-data; boundary=${boundary}`
+			}
+		},
+		Buffer.concat([
+			Buffer.from(data.join(''), 'utf8'),
+			Buffer.from(content, 'binary'),
+			Buffer.from(`\r\n--${boundary}--\r\n`, 'utf8')
+		])
+	).then(response =>
+	{
+		const statusCode = response.statusCode;
+		const colorNum = statusCode >= 400 ? 31 : (statusCode >= 300 ? 33 : 32);
+		const char = statusCode >= 400 ? '✖' : (statusCode >= 300 ? '⚠' : '✔');
+		console.log(
+			'\u001B[%dm%s\u001B[39m Server responded with \u001B[1;%dm%s\u001B[22m %s\u001B[39m\n\##[group]Additional info\n%s\n##[endgroup]',
+			colorNum,
+			char,
+			colorNum,
+			statusCode,
+			response.statusMessage,
+			response.data
+		);
+	}).catch(err =>
+	{
+		console.error(err);
+	});
+});
